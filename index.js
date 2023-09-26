c9b4f8458977e9a06776263499fe979ac67998b0
kaan <kabuke13@gmail.com>
2023-09-26 21:08:35 +0100

    I have now added an event listener that will show the front of a Pokemon card when the user hovers over the card

--- index.js
+++ index.js
@@ -75,6 +75,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
             //Append name, images and id to the front of card
             frontOfPokeCard.append(pokeName, pokeImg, pokeId)
+
+            //Now show the front of the card, when i hover over the back of it
+            backOfPokeCard.addEventListener("mouseover", () => {
+                pokeCardDiv.replaceChild(frontOfPokeCard, backOfPokeCard)
+            })
         }
         kantoPokemon.append(pokeCardDiv)
     }
